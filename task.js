5e8733827aacc2a5d874f202323201db55ab5d36
Saviourise <65181992+Saviourise@users.noreply.github.com>
2022-06-21 17:47:15 +0100

    Add files via upload

---
+++ task.js
@@ -0,0 +1,7 @@
+const name = "Saviour Okhuoya Ise",
+  height = "6ft",
+  country = "Nigeria";
+
+console.log(`My name is ${name}.`);
+console.log(`My height is ${height}.`);
+console.log(`I come from ${country}.`);
